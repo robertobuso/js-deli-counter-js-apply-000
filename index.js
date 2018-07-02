c66945b3a60b627d52f2f5626288c1b871b6a7cb
Roberto Busó-García <robertobuso@gmail.com>
2018-07-02 19:27:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine (katzDeliLine) {
+  var resultArray = [];
+  resultArray.push(katzDeliLine);
+  
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
