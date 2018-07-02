9cfb169fbcedbb612fb5bd4bc3b5eeaa74806258
Roberto Busó-García <robertobuso@gmail.com>
2018-07-02 19:28:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,17 +14,15 @@ function nowServing(katzDeliLine) {
 
 function currentLine (katzDeliLine) {
   var resultArray = [];
-  resultArray.push(katzDeliLine);
-  
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
     var i = 0;
     while (i < katzDeliLine.length){
-      katzDeliLine[i] = ` ${i+1}. ${katzDeliLine[i]}`;
+      resultArray.push(` ${i+1}. ${katzDeliLine[i]}`);
       i++;
     }
-    return `The line is currently:${katzDeliLine}`; 
+    return `The line is currently:${resultArray}`; 
   }
 }
 
